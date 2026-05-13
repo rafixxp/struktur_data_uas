5d83a5924596d422d18fe27b74f1a91f94a0b68b
rafixxp <xnuxerdev@gmail.com>
2026-05-13 14:57:59 +0700

    update: adding dequeue, displayAll, deleteAll function

--- main.cpp
+++ main.cpp
@@ -101,18 +101,92 @@ void enqueue(char *nama, int prioritas)
     printf("Nasabah %s berhasil ditambahkan ke dalam antrean, dengan nomor urut %d\n", nama, new_node->nomor_urut);
 }
 
+// Fungsi untuk memanggil nasabah berikutnya dari antrian (dari depan/head)
+void dequeue()
+{
+    if(isEmpty()){
+        printf("Antrian Kosong. Tidak ada nasabah yang akan dipanggil\n");
+    }
+    else{
+        printf("\n>>>> Memanggil Nasabah <<<<\n");
+        printf("No Urut : %d\n", head->nomor_urut);
+        printf("Nama    : %s\n", head->nama);
+        printf("Status  : %s\n", head->prioritas ? "Prioritas" : "Reguler");
+        printf("Silakan menuju ke teller yang telah di tunjukkan.\n");
+
+        if(head == tail){
+            delete head;
+            head = NULL;
+            tail = NULL;
+        }
+        else{
+            Node *temp = head;
+            head = head->next;
+            head->prev = tail;
+            tail->next = head;
+            delete temp;
+        }
+    }
+}
+
+// Fungsi untuk memanggil semua antrian nasabah
+void displayAll()
+{
+    if(isEmpty()){
+        printf("Antrian kosong, tidak ada nasabah yang terdaftar.\n");
+    }
+    else{
+        Node *current = head;
+        int posisi = 1;
+        printf("\n%-5s %-10s %-25s %-10s\n", "Antri", "No.Urut", "Nama Nasabah", "Status");
+        printf("----------------------------------------------------------------\n");
+
+        do{
+            printf("\n%-5d %-10d %-25s %-10s\n", posisi++, current->nomor_urut, current->nama, current->prioritas ? "Prioritas":"Reguler");
+            current = current->next;
+        }while(current != head);
+    }
+}
+
+// Fungsi untuk menghapus semua antrian nasabah
+void deleteAll()
+{
+    if(isEmpty()){
+        printf("Antrian kosong, tidak ada nasabah yang dapat dihapus !\n");
+    }
+    else{
+        tail->next = NULL;
+
+        Node *current = head;
+        while(current != NULL){
+            Node *temp = current;
+            current = current->next;
+            delete temp;
+        }
+
+        head = NULL;
+        tail = NULL;
+
+        counter_urut = 0;
+        printf("Semua data antrian nasabah berhasil dihapus !\n");
+    }
+}
+
 // Program Utama
 int main()
 {
     int choice;
     char nama[50];
     
-    while(choice != 3){
+    while(choice != 0){
         printf("\nProgram Antrian Nasabah Bank dengan Linked List\n");
         printf("=======================================================\n");
         printf("1. Daftarkan nasabah reguler\n");
         printf("2. Daftarkan nasabah prioritas\n");
-        printf("3. Keluar\n");
+        printf("3. Panggil Nasabah berikutnya\n");
+        printf("4. Tampilkan Semua Antrian Nasabah\n");
+        printf("5. Hapus Semua Antrian Nasabah\n");
+        printf("0. Keluar\n");
         printf("=======================================================\n");
         printf("> ");
         scanf("%d", &choice);
@@ -130,6 +204,15 @@ int main()
                 enqueue(nama, 1);
                 break;
             case 3:
+                dequeue();
+                continue;
+            case 4:
+                displayAll();
+                break;
+            case 5:
+                deleteAll();
+                break;
+            case 0:
                 printf("Terima kasih!");
                 break;
         }
